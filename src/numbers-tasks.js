a2638ff6345cd2abaf413bdcf6bc84b3bc5a1091
MaryTreasure <mari199191@mail.ru>
2024-11-09 19:08:27 +0300

    fix: linter fix

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -50,6 +50,7 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
+  // eslint-disable-next-line no-undef
   return (BigInt(value1) + BigInt(value2)) / BigInt(2);
 }
 
